69a0570b6bacf42fbe337d210dfd4cea90ce4e3f
callosaurus <callumdavies421@gmail.com>
2017-02-17 11:30:29 -0800

    first commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Callum Davies on 2017-02-17.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
